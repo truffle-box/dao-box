be708e06cf4fb7bbeac2d1841c1b74acde180e48
leeftk <lee.lara1219@gmail.com>
2022-10-25 16:23:30 -0300

    removed comments from propose

--- scripts/propose.js
+++ scripts/propose.js
@@ -9,17 +9,8 @@ const main = async (cb) => {
     //Create a proposal
     const receipt = await dao.propose([token.address], [1], ['0x'], 'hello world').then(async function (result) {
       console.log(result)
-      //Find the proposal ID by lookig through logs
-      const proposalId = result.receipt.logs.find((e) => e.event == 'ProposalCreated').args.proposalId;
-      console.log("Proposal Id Number:", BigInt(proposalId))
-      proposalNumber = BigInt(proposalId);
-      console.log('Proposal created 🦄: ', receipt.tx);
+      console.log('Proposal created 🦄: ', result.tx);
     });
-
-    //Check state of the proposal that was just created
-    const state = await dao.state(proposalNumber);
-    console.log("state:", BigInt(state))
-
   } catch (err) {
     console.log('Doh! ', err.message);
   }
